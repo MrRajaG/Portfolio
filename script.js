c8e431269ceec3b6deb6dc429b7c1f0fb9196b91
Raja Taha <rajataha062@gmail.com>
2023-06-04 17:25:50 +0500

    Next

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ console.log("Running");
 document.querySelector(".cross").style.display = "none";
 document.querySelector(".cheeseBurger").addEventListener("click", ()=>{
     document.querySelector(".sidebar").classList.toggle("sidebarGo");
+    if(document.querySelector(".sidebar").classList.contains("sidebarGo")){
+        document.querySelector(".cheese").style.display = "inline";
+        document.querySelector(".cross").style.display = "none";
+    }
+    else{
+        document.querySelector(".cheese").style.display = "none";
+        setTimeout(()=>{
+            document.querySelector(".cross").style.display = "inline";
+        }, 300);
+    }
 })
